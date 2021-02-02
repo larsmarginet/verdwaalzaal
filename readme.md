7e6b4e0bf9dc3f85b19b1fd02527ff58930c8b06
larsmarginet <lars.marginet@student.howest.be>
2021-02-02 13:59:21 +0100

    3-p5-perlin

--- readme.md
+++ readme.md
@@ -1,8 +1,4 @@
-# 2 - P5 & SCREENCAPTURE
+# DEMO 1
 
-This is a simple script to make a screencapture of a P5 sketch.
-Note that this captures the canvas and not just the screen. This means a framedrop, will not be seen in the final clip.
+Simple p5 script that reacts based on the data that is provided by an arduino potentiometer. This communication happens over websockets with P5.serialcontrol.
 
-The serial communication to the browser happens over websockets with a tool called [P5.serialcontrol](https://github.com/p5-serial/p5.serialport).
-
-On line 2 of the sketch the port is set to /dev/tty.usbmodem142101, but it might be a different one for you, so be sure to set it to the right one.
--- sketch.js
+++ sketch.js
@@ -1,18 +1,156 @@
+// https://github.com/wangyasai/Perlin-Noise/blob/gh-pages/js/sketch.js
+
 let serial; // variable to hold an instance of the serialport library
 let portName = '/dev/tty.usbmodem142101';
 let inData;
-
-let font, points;
-
-
 let capturer;
 let btn;
+
+
+let particles = [];
+let maxLife;
+
+
+let options = {  
+  Background : '#0a0a0a',
+  Color1 : '#cf4a4a',
+  Color2 : '#1cb3d4',
+  Color3 : '#ffd100',
+  Length : 5,
+  Nums : 400,
+  Size : 2,
+  noiseScale: 800,
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(options.Background);
+  // creates 2500 particles
+  for(var i = 0; i < 2500; i++){
+    particles[i] = new Particle();
+  }
+  arduino();
+  createButton();
+}
+
+
+
+function draw() {
+  noStroke();
+  smooth();  
+
+  maxLife = options.Length;
+  for(let i = 1; i < options.Nums; i++){
+      let iterations = map(i, 0, options.Nums, 5, 1);
+      // adjust thickness of lines
+      let radius = 2;
+      
+      particles[i].move(iterations);
+      particles[i].checkEdge();
+      
+      let alpha = 255;
+      let particleColor;
+      let fadeRatio;
+      fadeRatio = min(particles[i].life * 5 / maxLife, 1);
+      fadeRatio = min((maxLife - particles[i].life) * 5 / maxLife, fadeRatio);
+      
+      // Calculates the distance between particle and center 
+      let distance = dist(particles[i].pos.x, particles[i].pos.y, width/2, height/2);
+      // normalizes the distance between 0 and 400
+      let percent1 = norm(distance, 0, 400);
+      // normalizes the distance between 0 and the edge of the screen (x-axis)
+      let percent2 = norm(distance, 400, width/2);
+      from = color(options.Color1);
+      middle = color(options.Color2);
+      to = color(options.Color3);
+      between1 = lerpColor(from, middle, percent1);
+      between2 = lerpColor(middle, to, percent2);
+      if (distance < 400) {
+          particleColor = between1;
+      } else {
+          particleColor = between2;   
+      }    
+      fill(red(particleColor), green(particleColor), blue(particleColor), alpha * fadeRatio);
+      particles[i].display(radius);
+  } 
+  if (capturer) {
+    capturer.capture(document.getElementById("defaultCanvas0"));
+  }
+}
+
+function Particle(){
+  this.vel = createVector(0, 0);
+  this.pos = createVector(random(-50, width+50), random(-50, height+50));
+  this.life = random(0, 5);    
+  this.move = function(iterations){
+      if ((this.life -= 0.01666) < 0) this.respawn();
+
+      while (iterations > 0) {
+          var angle = noise(this.pos.x/options.noiseScale, this.pos.y/options.noiseScale)*TWO_PI*options.noiseScale;
+          this.vel.x = cos(angle);
+          this.vel.y = sin(angle);
+          // determines velocity of particle
+          this.vel.mult(.2);
+          this.pos.add(this.vel);
+          --iterations;
+      }
+  }
+
+  this.checkEdge = function(){
+      if(this.pos.x > width || this.pos.x < 0 || this.pos.y > height || this.pos.y < 0){
+          this.respawn();
+      }
+  }
+  
+  this.respawn = function(){
+      this.pos.x = random(-50, width+50);
+      this.pos.y = random(-50, height+50);
+      this.life = maxLife;
+  }
+
+  this.display = function(r){
+      ellipse(this.pos.x, this.pos.y, r, r);
+  }
+}
+
+// function touchStarted(){
+//   background(options.Background);
+//   for(var i = 0; i < options.Nums; i++){
+//       particles[i].respawn();
+//       particles[i].life = random(0,maxLife);
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(options.Background);
+}
+
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+}
+
 function record() {
   capturer = new CCapture({
     format: "webm",
     framerate: 24,
     verbose: true,
-    quality: 99, 
+    quality: 15, 
     timeLimit: 10,
   });
   capturer.start();
@@ -26,14 +164,18 @@ function record() {
   };
 }
 
-function preload() {
-	font = loadFont("assets/RobotoMono-Regular.ttf");
+const createButton = () => {
+  btn = document.createElement("button");
+  btn.textContent = "start recording";
+  document.body.appendChild(btn);
+  btn.style.display = 'none'
+  btn.onclick = record;
+  setTimeout(() => {
+    //btn.click(); //start recording automatically
+  }, 3000);
 }
 
-function setup() {
-  let p5Canvas = createCanvas(window.innerWidth, window.innerHeight);
-  background(0x08, 0x16, 0x40);
-  genType('MEMENTO', height / 4);
+const arduino = () => {
   serial = new p5.SerialPort();             // make a new instance of the serialport library
   serial.on('list', printList);             // set a callback function for the serialport list event
   serial.on('connected', serverConnected);  // callback for connecting to the server
@@ -44,43 +186,6 @@ function setup() {
  
   //serial.list();                          // list the serial ports
   serial.open(portName);                    // open a serial port
- 
-
-  btn = document.createElement("button");
-  btn.textContent = "start recording";
-  document.body.appendChild(btn);
-  btn.onclick = record;
-  setTimeout(() => {
-    btn.click(); //start recording automatically
-  }, 500);
-}
-
-
-
-function draw() {
-  background(0);
-	translate(width / 2, height / 2);
-
-	for(let i = 0; i < points.length; i++) {
-    let p = points[i];
-		let s = inData / 10 + sin(i * .25 + frameCount * .05) * 10;
-		circle(p.x, p.y, s);
-  }
-  
-  if (capturer) {
-    capturer.capture(document.getElementById("defaultCanvas0"));
-  }
-}
-
-function genType(txtString, txtSize) {
-	// grab bounding box of text
-	let bounds = font.textBounds(txtString, 0, 0, txtSize);
-
-	// textToPoints(txt, x, y, size, options)
-	points = font.textToPoints(txtString, -bounds.w / 2, bounds.h / 2, txtSize, {
-		sampleFactor: .07,
-		simplifyThreshold: 0
-	});
 }
 
 // get the list of ports:
